92595bcfdf5b3b751e35c68456a3c45b71d71111
Jack Guinane <jackguinane@gmail.com>
2022-07-30 23:39:43 -0700

    Actually read the docs this time

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,7 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-	baseURL: '/Sound-Utility/'
+	app: {
+		baseURL: '/Sound-Utility/'
+	}
 })
