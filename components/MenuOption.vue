4a9487010ca6682e533d8328fd50fb6af7400224
Jack Guinane <jackguinane@gmail.com>
2022-07-30 22:43:40 -0700

    Componentize menu items

---
+++ components/MenuOption.vue
@@ -0,0 +1,49 @@
+<template>
+	<li>
+		<NuxtLink :to="to">
+			<slot/>
+		</NuxtLink>
+	</li>
+</template>
+
+<script setup lang="ts">
+	defineProps<{ to: string }>()
+</script>
+
+<style scoped>
+	li
+	{
+		display: inline-block;
+		list-style: none;
+		padding-right: 0.5ch;
+		text-transform: uppercase;
+		font-size: 5rem;
+		font-weight: bold;
+		line-height: 1;
+		letter-spacing: -0.2em;
+	}
+
+	li::after
+	{
+		content: ",";
+	}
+
+	li:last-of-type::after
+	{
+		display: none;
+	}
+
+	li a, li::after
+	{
+		color: var(--accent);
+		-webkit-text-stroke: 1.5px var(--foreground);
+		text-decoration: none;
+	}
+
+	li a:hover,
+	li a:focus
+	{
+		color: var(--background);
+		outline: none;
+	}
+</style>
