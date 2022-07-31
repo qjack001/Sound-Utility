58a8310e31b49ce4ba63ae6f1258c98778697967
Jack Guinane <jackguinane@gmail.com>
2022-07-30 22:43:50 -0700

    Create new site logo

---
+++ components/Wordmark.vue
@@ -0,0 +1,27 @@
+<template>
+	<NuxtLink to="/">
+		Sound Utility
+	</NuxtLink>
+</template>
+
+<style scoped>
+	a
+	{
+		text-transform: uppercase;
+		font-weight: bold;
+		line-height: 1;
+		letter-spacing: -0.2em;
+		padding: 0 0.5ch;
+
+		color: var(--accent);
+		-webkit-text-stroke: 0.02em var(--foreground);
+		text-decoration: none;
+	}
+
+	a:hover,
+	a:focus
+	{
+		color: var(--background);
+		outline: none;
+	}
+</style>
