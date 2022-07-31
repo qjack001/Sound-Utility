b82ead09337040381bb54d4bb035be6746320378
Jack Guinane <jackguinane@gmail.com>
2022-07-30 22:45:37 -0700

    Refactor chord progression page

---
+++ pages/chords.vue
@@ -0,0 +1,42 @@
+<template>
+	<Head>
+		<Title>Simple Chord Progression | Sound Utility</Title>
+	</Head>
+	<p id="wordmark">
+		<Wordmark/>
+	</p>
+	<main>
+		<section id=info>
+			<h1>Simple Progression</h1>
+			<p>
+				Build a simple progression of simple chords. The three rows repersesent
+				the top three strings of the guitar (G-B-E).
+			</p>
+		</section>
+		<ChordProgressionBuilder/>
+	</main>
+</template>
+
+<script setup lang="ts">
+	import { MAJOR, Key, Scale, MINOR, SURFY } from '@/scripts/music-theory'
+
+
+	const key = ref<Key>(Key.G)
+	const scale = ref<Scale>(MAJOR)
+
+	
+</script>
+
+<style scoped>
+	#wordmark
+	{
+		font-size: 3.5rem;
+		margin: 0px 20px;
+	}
+
+	#info
+	{
+		max-width: 65ch;
+		margin: 60px 40px 20px;
+	}
+</style>
